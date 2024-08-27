366d33ab5d651e38e6d8d98dddd7bdf709818c54
Aleksandr Balakirev <santey3@gmail.com>
2024-08-27 16:43:25 +0200

    Updated to new passkeys version

--- corbado-auth.py
+++ corbado-auth.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_template, request, Response
+from typing import List
+from venv import logger
+from corbado_python_sdk.entities.session_validation_result import (
+    SessionValidationResult,
+)
+from corbado_python_sdk.generated.models.identifier import Identifier
+from flask import Flask, render_template, request
+from werkzeug.exceptions import Unauthorized
 from dotenv import load_dotenv
 import os
-from corbado_python_sdk import Config, CorbadoSDK, UserEntity, SessionInterface
+from corbado_python_sdk import (
+    Config,
+    CorbadoSDK,
+    IdentifierInterface,
+    UserEntity,
+    SessionInterface,
+    UserInterface,
+)
 
 
 # Load environment variables from .env file
@@ -22,6 +36,8 @@ config: Config = Config(
 # Initialize SDK
 sdk: CorbadoSDK = CorbadoSDK(config=config)
 sessions: SessionInterface = sdk.sessions
+identifiers: IdentifierInterface = sdk.identifiers
+users: UserInterface = sdk.users
 
 # Use the API_SECRET from the environment variables
 app.config["API_SECRET"] = API_SECRET
@@ -32,6 +48,7 @@ app.config["PROJECT_ID"] = PROJECT_ID
 
 @app.route("/")
 def login() -> str:
+    print("here2")
     return render_template(
         template_name_or_list="login.html", PROJECT_ID=app.config["PROJECT_ID"]
     )
@@ -39,20 +56,32 @@ def login() -> str:
 
 @app.route("/home")
 def home() -> str:
+    print("here")
     # Acquire cookies with your preferred method
     token: str = request.cookies.get(config.short_session_cookie_name) or ""
-    user: UserEntity = sessions.get_current_user(short_session=token)
+    validation_result: SessionValidationResult = sessions.get_current_user(
+        short_session=token
+    )
 
-    if user.authenticated:
-        user_data = {"id": user.user_id, "name": user.name, "email": user.email}
+    if validation_result.authenticated:
+        user: UserEntity = users.get(user_id=validation_result.user_id)
+        email_identifiers: List[Identifier] = identifiers.list_all_emails_by_user_id(
+            user_id=validation_result.user_id
+        )
+
+        user_data = {
+            "id": user.user_id,
+            "name": user.full_name,
+            "email": email_identifiers[0].value,
+        }
         return render_template(
             template_name_or_list="home.html",
             user_data=user_data,
             PROJECT_ID=app.config["PROJECT_ID"],
         )
     else:
-        return Response("You are not authenticated or have not yet confirmed your email.", status=401)
+        raise Unauthorized()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
